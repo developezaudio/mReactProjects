cece3ebee41087dd6e0ef356b7773c3736079a8e
Johnny Lopez <jalopez@berklee.edu>
2021-02-01 13:57:32 -0500

    Comment detail props edit and ApprovalCard created

---
+++ components/src/ApprovalCard.js
@@ -0,0 +1,7 @@
+import React from 'reeact';
+
+const ApprovalCard = () => {
+
+};
+
+export default ApprovalCard;
--- components/src/index.js
+++ components/src/index.js
@@ -5,7 +5,10 @@ import CommentDetail from './CommentDetail'
 const App = () => {
     return (
         <div className="ui container comments">
-        <CommentDetail author="sam"/>
+        <CommentDetail 
+            author="sam" 
+            timeAgo="today at 12:30pm"
+            commentText="Nice blog post!"/>
         </div>
     );
 };
