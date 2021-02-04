6379717cc9dcde478c3117fd670052b5b67c221d
Johnny Lopez <jalopez@berklee.edu>
2021-02-04 00:09:36 -0500

    new react app and finished components app

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-        <CommentDetail 
-            author="sam" 
-            timeAgo="today at 12:30pm"
-            commentText="Nice blog post!"/>
+        <ApprovalCard>
+            <CommentDetail 
+                author="sam" 
+                timeAgo="today at 12:30pm"
+                commentText="Nice blog post!"
+            />
+        </ApprovalCard>
         </div>
     );
 };
